0cee35a72bf5be47fa4b80e9c8ca84584517d5bb
vanshika <dviyapachani@gmail.com>
2023-06-29 17:55:34 +0530

    fisrt commit

---
+++ SUSU.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int i=1;
+clrscr();
+do{
+  if(i%2==0){ printf("%d\n",i);}
+  i++;
+  }while(i<=9);
+  getch();
+  }
---
+++ VANSHIKA.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int i=1;
+clrscr();
+do{
+  printf("%d\n",i);
+  i++;
+}while(i<=10);
+getch();
+}
